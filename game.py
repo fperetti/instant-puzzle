be2d02783df2c422f27233f30e9104c1fcc7ecf9
Federico M. Peretti <federicoperetti@gmail.com>
2016-06-28 08:13:16 -0300

    Now the puzzle can use a Image or Camera

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from kivy.uix.camera import Camera
 from kivy.uix.scatter import Scatter
 from kivy.animation import Animation
 from kivy.graphics import Color, Rectangle
@@ -9,57 +8,59 @@ from random import randint, random
 
 
 
-class Puzzle(Camera):
+def get_game_type(BaseClass):
 
-    blocksize = NumericProperty(100)
+    class Puzzle(BaseClass):
 
-    def on_texture_size(self, instance, value):
-        self.build()
+        blocksize = NumericProperty(100)
 
-    def on_blocksize(self, instance, value):
-        self.build()
+        def on_texture_size(self, instance, value):
+            self.build()
 
-    def build(self):
-        self.clear_widgets()
-        texture = self.texture
-        if not texture:
-            return
-        bs = self.blocksize
-        tw, th = self.texture_size
-        for x in range(int(tw / bs)):
-            for y in range(int(th / bs)):
-                bx = x * bs
-                by = y * bs
-                subtexture = texture.get_region(bx, by, bs, bs)
-                # node = PuzzleNode(texture=subtexture,
-                #                  size=(bs, bs), pos=(bx, by))
-                node = Scatter(pos=(bx, by), size=(bs, bs))
-                with node.canvas:
-                    Color(1, 1, 1)
-                    Rectangle(size=node.size, texture=subtexture)
-                self.add_widget(node)
+        def on_blocksize(self, instance, value):
+            self.build()
 
-        self.shuffle()
+        def build(self):
+            self.clear_widgets()
+            texture = self.texture
+            if not texture:
+                return
+            bs = self.blocksize
+            tw, th = self.texture_size
+            for x in range(int(tw / bs)):
+                for y in range(int(th / bs)):
+                    bx = x * bs
+                    by = y * bs
+                    subtexture = texture.get_region(bx, by, bs, bs)
+                    node = Scatter(pos=(bx, by), size=(bs, bs))
+                    with node.canvas:
+                        Color(1, 1, 1)
+                        Rectangle(size=node.size, texture=subtexture)
+                    self.add_widget(node)
 
-    def shuffle(self):
-        texture = self.texture
-        bs = self.blocksize
-        tw, th = self.texture_size
-        count = int(tw / bs) * int(th / bs)
-        indices = list(range(count))
-        childindex = 0
-        while indices:
-            index = indices.pop(randint(0, len(indices) - 1))
-            x = bs * (index % int(tw / bs))
-            y = bs * int(index / int(tw / bs))
-            child = self.children[childindex]
-            a = Animation(d=random() / 4.) + Animation(pos=(
-                x, y), t='out_quad', d=.4)
-            a.start(child)
-            childindex += 1
-
-    def on_touch_down(self, touch):
-        if touch.is_double_tap:
             self.shuffle()
-            return True
-        super(Puzzle, self).on_touch_down(touch)
+
+        def shuffle(self):
+            texture = self.texture
+            bs = self.blocksize
+            tw, th = self.texture_size
+            count = int(tw / bs) * int(th / bs)
+            indices = list(range(count))
+            childindex = 0
+            while indices:
+                index = indices.pop(randint(0, len(indices) - 1))
+                x = bs * (index % int(tw / bs))
+                y = bs * int(index / int(tw / bs))
+                child = self.children[childindex]
+                a = Animation(d=random() / 4.) + Animation(pos=(
+                    x, y), t='out_quad', d=.4)
+                a.start(child)
+                childindex += 1
+
+        def on_touch_down(self, touch):
+            if touch.is_double_tap:
+                self.shuffle()
+                return True
+            super(Puzzle, self).on_touch_down(touch)
+
+    return Puzzle
--- main.py
+++ main.py
@@ -8,64 +8,77 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
-from game import Puzzle
+from kivy.uix.camera import Camera
 
-
+import random
+import glob
+import game
 
 
 class PuzzleApp(App):
 
-	def __init__(self, **kwargs):
-		super(PuzzleApp, self).__init__(**kwargs)
-		self.start_game = False
-		self.background_sound = SoundLoader.load('assets/audio/music.ogg')
-		self.background_sound.loop = True
-		self.content = Widget()
+    def __init__(self, **kwargs):
+        super(PuzzleApp, self).__init__(**kwargs)
+        self.start_game = False
+        self.background_sound = SoundLoader.load('assets/audio/music.ogg')
+        self.background_sound.loop = True
+        self.content = Widget()
+        self.type = 'IMAGE'
 
-	def build(self):
-		root = Widget()
-		center_x = Window.width/2
-		center_y = Window.height/2
-		button_start_game = Button(text='Start Game',pos=(center_x-100, center_y+100),size=(200, 100))
-		button_options = Button(text='Options',pos=(center_x-100, center_y),size=(200, 100))
-		button_quit = Button(text='Quit',pos=(center_x-100, center_y-100),size=(200, 100))
+    def build(self):
+        root = Widget()
+        center_x = Window.width / 2
+        center_y = Window.height / 2
+        button_start_game = Button(text='Start Game', pos=(
+            center_x - 100, center_y + 100), size=(200, 100))
+        button_options = Button(text='Options', pos=(
+            center_x - 100, center_y), size=(200, 100))
+        button_quit = Button(text='Quit', pos=(
+            center_x - 100, center_y - 100), size=(200, 100))
 
-		button_start_game.bind(on_press=self.start)
-		#button_options.bind(on_press=self.options)
-		button_quit.bind(on_press=self.quit)
+        button_start_game.bind(on_press=self.start)
+        # button_options.bind(on_press=self.options)
+        button_quit.bind(on_press=self.quit)
 
-		self.background = Image(source = 'assets/menu_background.jpg')
-		self.background.size = (Window.width,Window.height)
+        self.background = Image(source='assets/graphics/menu_background.jpg')
+        self.background.size = (Window.width, Window.height)
 
-		self.backgroundopacity = 0.35
-		self.content.add_widget(self.background)
+        self.backgroundopacity = 0.35
+        self.content.add_widget(self.background)
 
-		self.content.add_widget(button_start_game)
-		self.content.add_widget(button_options)
-		self.content.add_widget(button_quit)
+        self.content.add_widget(button_start_game)
+        self.content.add_widget(button_options)
+        self.content.add_widget(button_quit)
 
-		return self.content
+        return self.content
 
-	def on_value(self, puzzle, instance, value):
-		value = int((value + 5) / 10) * 10
-		puzzle.blocksize = value
-		instance.value = value
+    def on_value(self, puzzle, instance, value):
+        value = int((value + 5) / 10) * 10
+        puzzle.blocksize = value
+        instance.value = value
 
-	def start(self, value):
-		print("Start Game")
-		self.background_sound.play()
-		self.content.clear_widgets()
-		puzzle = Puzzle(resolution=(640, 480), play=True)
-		slider = Slider(min=100, max=200, step=10, size=(800, 50))
-		slider.bind(value=partial(self.on_value, puzzle))
-		self.content.add_widget(puzzle)
-		self.content.add_widget(slider)
+    def start(self, value):
+        print("Start Game")
+        self.background_sound.play()
+        self.content.clear_widgets()
 
-	def options(self,value):
-		print(value)
+        if(self.type == 'IMAGE'):
+            puzzles_images = glob.glob( "assets/graphics/puzzles/*.jpg" )
+            puzzle = game.get_game_type(Image)(source=random.choice(puzzles_images))
+        else:
+            puzzle = game.get_game_type(Camera)(resolution=(640, 480), play=True)
 
-	def quit(self,value):
-		PuzzleApp().get_running_app().stop()
+
+        slider = Slider(min=100, max=200, step=10, size=(800, 50))
+        self.content.add_widget(puzzle)
+        slider.bind(value=partial(self.on_value, puzzle))
+        self.content.add_widget(slider)
+
+    def options(self, value):
+        print(value)
+
+    def quit(self, value):
+        PuzzleApp().get_running_app().stop()
 
 
 PuzzleApp().run()
